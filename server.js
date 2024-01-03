aa90e1ff5ecd4b03d08750bd9ae11b57216acf63
Colton Skiera <cskiera142@gmail.com>
2024-01-03 15:21:50 -0700

    inital server.js build - up and running server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+// Middleware //
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+
+// Routes //
 app.get("/", (req, res) => {
   try {
     res.json("Get Request");
